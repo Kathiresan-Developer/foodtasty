58a29d18be194c33bc6d36f350cc936ce0e22d44
sreedhar <ssreedhar92@gmail.com>
2025-01-11 17:55:24 +0530

    change

--- lib/constants.dart
+++ lib/constants.dart
@@ -11,16 +11,3 @@ const bgImage = "assets/food.jpg";
 const loginScreen = "Login";
 
 const forgotText = "Fogot Password";
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
-
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
